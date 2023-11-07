9aefd86b93fc48292c41d42fbf1b9725dd70da9a
gitbatu <34317181+gitbatu@users.noreply.github.com>
2023-11-06 19:54:55 -0700

    Update libvpx.cmake
    Changed tag

--- cmake/libvpx.cmake
+++ cmake/libvpx.cmake
@@ -1,2 +1,1 @@
-import_remote("https://chromium.googlesource.com/webm/libvpx" TAG "master" NO_CMAKE ALIAS libvpx_source)
-
+import_remote("https://chromium.googlesource.com/webm/libvpx" TAG "v1.13.1" NO_CMAKE ALIAS libvpx_source)
